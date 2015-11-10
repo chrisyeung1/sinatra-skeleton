cbd198597889777eb639d04d456112766128b3a7
chrisyeung1 <chrisyeung@alumni.ubc.ca>
2015-11-09 19:21:45 -0800

    Migrating login and profile page

---
+++ Day9.rb
@@ -0,0 +1,6 @@
+# schema is the current snap shot of the database and the tables
+# model always singular and capitalized
+# child table always references the parent table
+# tux allows us to go into back end of our model
+#   get an idea of if everything is working properly
+#   
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ActiveRecord::Base
-
+  def change
+    create_table :movies do |t|
+      t.string
 
 end
